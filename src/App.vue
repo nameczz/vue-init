9fde7149292946eaa8afcda218a650d90d5aef0f
nameczz <nameczz@126.com>
2017-07-09 21:44:15 +0800

    vue-init
    vue的初始化模版，方便工作

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<template>
+    <div id="app">
+        <admin v-if="login">
+
+        </admin>
+        <div v-else>
+            <tab :fathers="fathers" :children="childrens"></tab>
+        </div>
+        <router-view></router-view>
+    </div>
+</template>
+
+<script>
+import tab from 'components/tab'
+import admin from 'components/admin/admin'
+
+export default {
+    name: 'app',
+    data() {
+        return {
+            fathers: ['1', '2', '3'],
+            childrens: [
+                ['1', '2', '3'],
+                ['1', '2', '3'],
+                ['1', '2', '3']
+            ],
+            login: true
+        }
+    },
+    components: {
+        tab,
+        admin
+    }
+}
+</script>
+
+<style>
+
+</style>
